ef52516f5e791180ed615610de85f1eaac26d31a
Pranay Uppuluri <pcu000@gmail.com>
2012-09-20 23:05:53 -0400

    Edited development and production default domain url settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,8 +48,8 @@ SampleApp::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
-  # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options[:host] = "rocky-anchorage-8306.herokuapp.com"
 
   # Enable threaded mode
   # config.threadsafe!
