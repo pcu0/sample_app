dd74d38c20e1f2d4ba485673aded519d9d7edc2a
Pranay Uppuluri <pcu000@gmail.com>
2012-09-20 22:43:29 -0400

    Rearranged flash messages in passwords reset controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -7,8 +7,8 @@ class PasswordResetsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user
       user.send_password_reset
-      flash[:success] = "Email sent with password reset instructions."
       redirect_to root_url
+      flash[:success] = "Email sent with password reset instructions."
     else
       flash[:error] = "There is no member with that email."
     end
@@ -21,11 +21,11 @@ class PasswordResetsController < ApplicationController
   def update
     @user = User.find_by_password_reset_token!(params[:id])
     if @user.password_reset_sent_at < 2.hours.ago
-      flash[:error] = "Password reset has expired."
       redirect_to new_password_reset_path
+      flash[:error] = "Password reset has expired."
     elsif @user.update_attributes(params[:user])
-      flash[:success] = "Password has been reset!"
       redirect_to root_url
+      flash[:success] = "Password has been reset!"
     else
       render :edit
     end
