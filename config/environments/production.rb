d7b67caf1c8cb6dd31ed9cd03229018e0c3d0a21
Pranay Uppuluri <pcu000@gmail.com>
2012-09-20 23:21:26 -0400

    Reversed changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,8 +48,8 @@ SampleApp::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options[:host] = "rocky-anchorage-8306.herokuapp.com"
+  #config.action_mailer.raise_delivery_errors = true
+  #config.action_mailer.default_url_options[:host] = "rocky-anchorage-8306.herokuapp.com"
 
   # Enable threaded mode
   # config.threadsafe!
